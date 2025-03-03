03a4e1b214f100aedc3ae5c8ed7e36a97db3fb1b
Thay.min <134392865+thayzavi@users.noreply.github.com>
2025-03-03 18:29:18 +0000

    json

--- anime-search/server.js
+++ anime-search/server.js
@@ -1,26 +1,24 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
 const fs = require('fs');
 const path = require('path'); // Adicionado
 
 // Carregamento do JSON
-const animes = JSON.parse(fs.readFileSync('./animes.json'));
-console.log(animes);
-
-// Servir arquivos estáticos
-app.use(express.static(__dirname));
-
 app.get('/search', (req, res) => {
+    const animes = JSON.parse(fs.readFileSync('./animes.json')); // Lendo os dados do JSON
     const searchQuery = req.query.q ? req.query.q.toLowerCase() : '';
+
+    // Filtrando animes pelo nome ou descrição
     const filteredAnimes = animes.filter(anime => 
         anime.nome.toLowerCase().includes(searchQuery) ||
         anime.descricao.toLowerCase().includes(searchQuery)
     );
+
     res.json(filteredAnimes);
 });
 
-// Rota para servir index.html corretamente
+// Rota para servir pesquisar.html corretamente
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'pesquisar.html'));
 });
